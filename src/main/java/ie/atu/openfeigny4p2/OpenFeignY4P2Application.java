9b1248a0e3fd63f81ed10bc2e7a33b72917a1516
rossa <g00401655@atu.ie>
2024-10-05 15:17:39 +0100

    First PUSH

---
+++ src/main/java/ie/atu/openfeigny4p2/OpenFeignY4P2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeigny4p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignY4P2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignY4P2Application.class, args);
+    }
+
+}
